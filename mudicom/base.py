e9f5296b885ec406b5f4a50d281d515d9bc52619
Eric Bower <neurosnap@gmail.com>
2014-05-26 16:11:11 -0400

    got images to save!

--- mudicom/base.py
+++ mudicom/base.py
@@ -5,21 +5,21 @@ import os
 import gdcm
 
 class BaseDicom(object):
-	
-	def __init__(self, fname):
-		self.fname = fname
-		self.file_name, self.file_extension = os.path.splitext(fname)
-		self.gdcm = gdcm
-		
-		reader = gdcm.Reader()
-		reader.SetFileName(fname)
-		if not reader.Read():
-			raise Exception("Not a valid DICOM file")
 
-		self.file = reader.GetFile()
+    def __init__(self, fname):
+        self.fname = fname
+        self.file_name, self.file_extension = os.path.splitext(fname)
+        self.gdcm = gdcm
 
-		self.str_filter = gdcm.StringFilter()
-		self.str_filter.SetFile(reader.GetFile())
+        reader = gdcm.Reader()
+        reader.SetFileName(fname)
+        if not reader.Read():
+            raise Exception("Not a valid DICOM file")
 
-	def __repr__(self):
-		return repr(self.fname)
+        self.file = reader.GetFile()
+
+        self.str_filter = gdcm.StringFilter()
+        self.str_filter.SetFile(reader.GetFile())
+
+    def __repr__(self):
+        return repr(self.fname)
--- mudicom/image.py
+++ mudicom/image.py
@@ -2,6 +2,7 @@ from __future__ import division
 import os.path
 import numpy
 import gdcm
+
 from .base import BaseDicom
 from .reader import Reader
 
@@ -11,49 +12,65 @@ class Image(BaseDicom):
     def __init__(self, fname):
         super(Image, self).__init__(fname)
 
-    def get_image(self):
+    def get_numpy(self):
         """ Extract image from DICOM file """
-        file_reader = Reader(self.fname)
-        dataset = file_reader.get_dataset()
-
+        # load GDCM's image reading functionality
         image_reader = self.gdcm.ImageReader()
+        image_reader.SetFileName(self.fname)
+        if not image_reader.Read():
+            raise Exception("Could not read DICOM image")
         pixel_array = gdcm_to_numpy(image_reader.GetImage())
-        # save full image
-        pil_image = Image.fromarray(pixel_format)
-        pil_image.save(os.path.join(self.file_name, ".png"))
+        return pixel_array
+
+    def save_image_plt(self, fname, pixel_array, vmin=None, vmax=None, 
+        cmap=None, format=None, origin=None):
+        """ Requires matplotlib """
+        from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+        from matplotlib.figure import Figure
+        from pylab import cm
+        if cmap is None:
+            cmap = cm.bone
+
+        fig = Figure(figsize=pixel_array.shape[::-1], dpi=1, frameon=False)
+        #fig.set_size_inches(5.12, 5.12)
+        canvas = FigureCanvas(fig)
+        fig.figimage(pixel_array, cmap=cmap, vmin=vmin, vmax=vmax, origin=origin)
+        fig.savefig(fname, dpi=1, format=format)
+        return True
+
+    def save_image_pil(self, fname, pixel_array):
+        """ Requires PIL """
+        from PIL import Image as pillow
+
+        pil_image = pillow.fromarray(pixel_array.astype('uint8'))
+        pil_image.save(fname)
         # save thumbnail of image
-        pil_image.thumbnail((150,150,), Image.ANTIALIAS)
-        pil_image.save(os.path.join(self.file_name, "_thumb.png"), "PNG")
+        #pil_image.thumbnail((150,150,), pillow.ANTIALIAS)
+        #pil_image.save(''.join([file_name, "_thumb.png"]), "PNG")
+        return True
 
 def gdcm_to_numpy(image):
     """ Converts a GDCM image to a numpy array. """
     gdcm_typemap = {
-        gdcm.PixelFormat.UINT8:     numpy.int8,
-        gdcm.PixelFormat.INT8:      numpy.uint8,
-        gdcm.PixelFormat.UINT16:    numpy.uint16,
-        gdcm.PixelFormat.INT16:     numpy.int16,
-        gdcm.PixelFormat.UINT32:    numpy.uint32,
-        gdcm.PixelFormat.INT32:     numpy.int32,
-        gdcm.PixelFormat.FLOAT32:   numpy.float32,
-        gdcm.PixelFormat.FLOAT64:   numpy.float64 
+        gdcm.PixelFormat.INT8:     numpy.int8,
+        gdcm.PixelFormat.UINT8:    numpy.uint8,
+        gdcm.PixelFormat.UINT16:   numpy.uint16,
+        gdcm.PixelFormat.INT16:    numpy.int16,
+        gdcm.PixelFormat.UINT32:   numpy.uint32,
+        gdcm.PixelFormat.INT32:    numpy.int32,
+        gdcm.PixelFormat.FLOAT32:  numpy.float32,
+        gdcm.PixelFormat.FLOAT64:  numpy.float64 
     }
     pixel_format = image.GetPixelFormat().GetScalarType()
     if pixel_format in gdcm_typemap:
         data_type = gdcm_typemap[pixel_format]
-        print(data_type)
     else:
         raise KeyError(''.join(pixel_format, " is not a supported pixel format"))
 
-    # dimension = image.GetDimension(0), image.GetDimension(1)
-    dimension = image.GetDimension(1), image.GetDimension(0)
+    #dimension = image.GetDimension(0), image.GetDimension(1)
+    dimensions = image.GetDimension(1), image.GetDimension(0)
     gdcm_array = image.GetBuffer()
     ## use float for accurate scaling
     result = numpy.frombuffer(gdcm_array, dtype=data_type).astype(float)
-    ## optional gamma scaling
-    maxV = float(result[result.argmax()])
-    result = result + 0.5 * (maxV - result)
-    result = numpy.log(result + 50) ## apprx background level
-
-    result.shape = dimension
-
+    result.shape = dimensions
     return result
