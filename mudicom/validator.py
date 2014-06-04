b3115fab3cc2300e8f5fa9aedbe80aa49cb0433b
Eric Bower <neurosnap@gmail.com>
2014-06-03 21:47:34 -0400

    figuring out validator

--- mudicom/validator.py
+++ mudicom/validator.py
@@ -2,4 +2,16 @@ from .base import BaseDicom
 
 
 class Validator(BaseDicom):
-	pass
+
+    def __init__(self):
+        cmd = ["dciodvfy", self.filename]
+        self.output, self.error = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+
+    def errors():
+        pass
+
+    def warnings():
+        pass
+
+    def all():
+        pass
