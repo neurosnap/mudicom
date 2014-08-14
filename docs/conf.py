db9a194f012af61e180061853b49ae7c4124df87
Eric Bower <neurosnap@gmail.com>
2014-08-14 09:43:41 -0400

    sphinx rtd theme not worky

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,12 @@
 
 import sys
 import os
+on_rtd = os.environ.get("READTHEDOCS", None) == 'True'
 
-import sphinx_rtd_theme
+if not on_rtd:
+	import sphinx_rtd_theme
+	html_theme = 'sphinx_rtd_theme'
+	html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
